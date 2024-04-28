fad47d27788f43754bd19bd5376d5902416ad66e
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-04-28 12:44:39 +0400

    Updated sponsors logos

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-4 lg:mt-10 px-4 lg:px-0">
-    <div class="text-center">
+    <div class="text-center mb-24">
         <h2 class="text-md lg:text-3xl font-pixel">We're backed by some big players</h2>
         <div class="mt-2 lg:mt-4 text-mg lg:text-xl lg:text-2xl text-center font-pixel font-bold text-yellow-400">
             Sponsors & Partners
@@ -38,9 +38,15 @@
             <img src="/images/sponsor-tiers/gold.svg" class="h-8 lg:h-10" />
         </div>
         <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
-            <div class="flex flex-col lg:flex-row gap-8 items-center">
-                <img src="/images/sponsors/elca.png" class="h-6 lg:h-10" />
-                <img src="/images/sponsors/astek.png" class="h-20 lg:h-28" />
+            <div class="flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/elca.png" class="h-8 lg:h-12" />
+                <img src="/images/sponsors/astek.png" class="h-24 lg:h-30" />
+                <img src="/images/sponsors/eventstore.svg" class="h-12 lg:h-16" />
+                <img src="/images/sponsors/iqera.png" class="h-20 lg:h-26" />
+            </div>
+            <div class="mt-10 flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/publicis.png" class="h-20 lg:h-24" />
+                <img src="/images/sponsors/klanik.png" class="h-20 lg:h-24" />
             </div>
         </div>
 
@@ -57,11 +63,67 @@
             </div>
         </div>
 
-        <div class="mt-8 lg:mt-16 mb-24 max-w-md mx-auto flex flex-row justify-between gap-4">
+        <div class="mt-8 lg:mt-16 max-w-md mx-auto flex flex-row justify-between gap-4">
             <!-- Bronze Tier -->
             <img src="/images/sponsor-tiers/bronze.svg" class="h-8 lg:h-10" />
             <h3 class="text-lg lg:text-2xl font-pixel font-bold">Bronze Tier</h3>
             <img src="/images/sponsor-tiers/bronze.svg" class="h-8 lg:h-10" />
         </div>
+        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+            <div class="flex flex-col lg:flex-row gap-6">
+                <img src="/images/sponsors/ios.png" class="h-16 lg:h-20" />
+            </div>
+        </div>
+
+        <div class="mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
+            <!-- Speaker Sponsor -->
+            <h3 class="text-lg lg:text-2xl font-pixel font-bold">Speaker Sponsor</h3>
+        </div>
+        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+            <div class="flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/ringier.png" class="h-10 lg:h-12" />
+            </div>
+        </div>
+
+        <div class="mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
+            <!-- Networking Hour Sponsor -->
+            <h3 class="text-lg lg:text-2xl font-pixel font-bold">Networking Hour Sponsor</h3>
+        </div>
+        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+            <div class="flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/spoonconsulting.png" class="h-16 lg:h-20" />
+            </div>
+        </div>
+
+        <div class="mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
+            <!-- Internet Sponsor -->
+            <h3 class="text-lg lg:text-2xl font-pixel font-bold">Internet Sponsor</h3>
+        </div>
+        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+            <div class="flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/kaldera.png" class="h-10 lg:h-12" />
+            </div>
+        </div>
+
+        <div class="mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
+            <!-- Media Partner -->
+            <h3 class="text-lg lg:text-2xl font-pixel font-bold">Media Partner</h3>
+        </div>
+        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+            <div class="flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/lasentinelle.svg" class="h-8 lg:h-10" />
+            </div>
+        </div>
+
+        <div class="mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
+            <!-- Hosting Sponsor -->
+            <h3 class="text-lg lg:text-2xl font-pixel font-bold">Hosting Sponsor</h3>
+        </div>
+        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+            <div class="flex flex-col lg:flex-row gap-10 items-center">
+                <img src="/images/sponsors/cloud-mu.png" class="h-10 lg:h-12" />
+            </div>
+        </div>
+
     </div>
 </div>
