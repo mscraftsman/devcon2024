a4400167518a1f231079e2f8cd91fcf89ee22d5c
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-04-30 09:04:04 +0400

    Updated sponsors page

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -21,7 +21,7 @@
         <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
             <div class="flex flex-col lg:flex-row gap-6">
                 <a href="https://www.swanforlife.com/" target="_blank">
-                    <img src="/images/sponsors/swan.png" class="h-8 lg:h-12" />
+                    <img src="/images/sponsors/swan.png" class="h-10 lg:h-12" />
                 </a>
             </div>
         </div>
@@ -47,7 +47,7 @@
             <img src="/images/sponsor-tiers/gold.svg" class="h-8 lg:h-10" />
         </div>
         <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
-            <div class="flex flex-col lg:flex-row gap-16 items-center">
+            <div class="flex flex-col lg:flex-row gap-8 lg:gap-16 items-center">
                 <a href="https://careers.elca.mu/" target="_blank">
                     <img src="/images/sponsors/elca.png" class="h-8 lg:h-10" />
                 </a>
@@ -61,12 +61,15 @@
                     <img src="/images/sponsors/iqera.png" class="h-20 lg:h-26" />
                 </a>
             </div>
-            <div class="mt-10 flex flex-col lg:flex-row gap-16 items-center">
+            <div class="mt-10 flex flex-col lg:flex-row gap-8 lg:gap-16 items-center">
                 <a href="https://www.publicisgroupe.com" target="_blank">
                     <img src="/images/sponsors/publicis.png" class="h-20 lg:h-24" />
                 </a>
-                <a href="https://www.klanik.com/" target="_blank">
+                <a class="hidden" href="https://www.klanik.com/" target="_blank">
                     <img src="/images/sponsors/klanik.png" class="h-20 lg:h-32" />
+                </a>
+                <a href="https://www.teamengine.com/" target="_blank">
+                    <img src="/images/sponsors/team-engine.png" class="h-12 lg:h-16" />
                 </a>
             </div>
         </div>
@@ -78,7 +81,7 @@
             <img src="/images/sponsor-tiers/silver.svg" class="h-8 lg:h-10" />
         </div>
         <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
-            <div class="flex flex-col lg:flex-row gap-16 items-center">
+            <div class="flex flex-col lg:flex-row gap-8 lg:gap-16 items-center">
                 <a href="https://www.rapp.com/" target="_blank">
                     <img src="/images/sponsors/rapp.png" class="h-6 lg:h-8" />
                 </a>
@@ -136,11 +139,11 @@
             </div>
         </div>
 
-        <div class="mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
+        <div class="hidden mt-8 lg:mt-16 max-w-md mx-auto grid place-items-center">
             <!-- Media Partner -->
             <h3 class="text-lg lg:text-2xl font-pixel font-bold">Media Partner</h3>
         </div>
-        <div class="mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
+        <div class="hidden mt-8 lg:mt-16 max-w-4xl mx-auto grid place-items-center">
             <div class="flex flex-col lg:flex-row gap-10 items-center">
                 <a href="https://lasentinelle.mu" target="_blank">
                     <img src="/images/sponsors/lasentinelle.svg" class="h-8 lg:h-10" />
