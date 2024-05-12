73f11422b060dacbd8cbb6a24f726dc181f70053
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-05-12 22:31:04 +0400

    Rename the main image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="relative">
-        <img src="/images/main.png" class="w-full" />
+        <img src="/images/main-2024.png" class="w-full" />
         <div class="mt-2 lg:-mt-36 font-pixel text-center text-md lg:text-2xl text-[#E83F3B] lg:text-gray-50">
             <p>18, 19 & 20 July</p>
             <p>Caudan Arts Centre</p>
