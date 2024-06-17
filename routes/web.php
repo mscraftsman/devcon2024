0d7864c7ca55f0bb179838f4be8300209b2bf69f
Nirvan Pagooah <tejas@pagooah.com>
2024-06-17 04:01:33 +0400

    fix: SEO

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,24 @@ use App\Http\Controllers\SpeakerController;
 use App\Http\Controllers\SpeakersController;
 
 Route::get('/', function () {
-    return view('index');
-});
+    $title = 'MSCC — Developers Conference 2024';
+    return view('index', compact('title'));
+})->name('index');
 
 Route::get('/code-of-conduct', function () {
-    return view('code-of-conduct');
-});
+    $title = 'MSCC — Code of Conduct';
+     return view('conduct', compact('title'));
+})->name('code-of-conduct');
 
 Route::get('/community', function () {
-    return view('community');
-});
-
-// Route::get('/agenda', function () {
-//     return view('index');
-// });
+    $title = 'MSCC — Community';
+    return view('conduct', compact('title'));
+})->name('community');
 
 Route::get('/speakers', [SpeakersController::class, 'index'])->name('speakers');
 Route::get('/speaker/{id}', [SpeakerController::class, 'searchById'])->name('speaker');
 
-Route::get('/agenda/{id}', [AgendaSingleController::class, 'searchSessionById'])->name('session');
 Route::get('/agenda', [AgendaController::class, 'index'])->name('agenda');
+Route::get('/agenda/{id}', [AgendaSingleController::class, 'searchSessionById'])->name('session');
 
 require __DIR__.'/auth.php';
